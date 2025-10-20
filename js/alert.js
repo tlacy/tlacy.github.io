dbd5766d12c5fb062bdb141471c93c869a499f5b
tlacy <tlacy@users.noreply.github.com>
2025-10-20 17:48:53 +0000

    deploy: 46b213c370352a44003e16352fcd9ef4164cf029

--- js/alert.js
+++
@@ -1,5 +0,0 @@
-/**
- * alert.js preserved for historical reasons but no longer shows blocking alert on page load.
- * If you need lightweight notifications, use the non-blocking showMessage helper in js/chat.js
- */
-
