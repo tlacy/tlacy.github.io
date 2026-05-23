a63c87cd426d7b4d864c107e8a1912888a7e42a7
Tom Lacy <tomalacy@gmail.com>
2026-05-23 18:47:01 -0500

    Add PhotoCraft app at /photocraft

---
+++ photocraft/app.js
@@ -0,0 +1,6 @@
+// PhotoCraft app.js
+// %%APP_VERSION%%
+// This file intentionally left as a companion JS file for any
+// standalone scripts that don't fit in app.html's inline script.
+// The main application logic lives in the <script> tag of app.html.
+// Deploy: deploy-frontend.sh replaces %%APP_VERSION%% before syncing.
